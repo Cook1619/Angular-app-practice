9bb321d8df9eb55e06cdd732dd2afc6a1c26de48
Cook1619 <cook6348@gmail.com>
2019-03-05 12:34:16 -0600

    Playing around with types in typescript

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -8,27 +8,52 @@ import { userInfo } from 'os';
 })
 
 export class UserComponent {
-  firstName = 'John';
-  lastName = 'Doe';
-  age = 30;
-  address = {
-    street: '123 Main St',
-    city: 'Mayer',
-    state: 'Minnesota'
-  };
+  firstName: string;
+  lastName: string;
+  age: number;
+  address;
+
+  foo: any;
+  hasKids: boolean;
+  // this makes the numberArray variable expect an array of numbers nothing else
+  numberArray: number[];
+  // same concept but with strings
+  stringArray: string[];
+  // can hold any values
+  mixedArray: any[];
+  // defining each index in the array, after the defined amounts you can have whatever data you want without throwing errors
+  myTuple: [string, number, boolean];
+
+  u: undefined;
+  n: null;
+
   // runs automatically when object is instanciated
   constructor() {
-    this.sayHello();
-    console.log(this.age);
-    this.hasBirthday();
-    console.log(this.age);
+    this.firstName = 'John';
+    this.lastName = 'Doe';
+    this.age =  30;
+    this.address = {
+      street: '123 Main St',
+      city: 'Mayer',
+      state: 'Minnesota'
+    };
+    this.numberArray = [1, 2, 3];
+    this.myTuple = ['hello', 4, true];
+    this.u = undefined;
+    this.n = null;
+
+    console.log(this.addNumbers(2, 3));
   }
 
-  sayHello() {
-    console.log(`Hello ${this.firstName}`);
-  }
+  // sayHello() {
+  //   console.log(`Hello ${this.firstName}`);
+  // }
 
-  hasBirthday() {
-    this.age += 1;
+  // hasBirthday() {
+  //   this.age += 1;
+  // }
+
+  addNumbers(num1: number, num2: number): number{
+    return num1 + num2;
   }
 }
