609ed4fbf95740acf1216a01a0abcfdbc07cf241
Cook1619 <cook6348@gmail.com>
2019-03-05 12:53:05 -0600

    Created a interface, mapped object which we can bring in and use whereever we please

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { userInfo } from 'os';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-user',
@@ -8,52 +8,19 @@ import { userInfo } from 'os';
 })
 
 export class UserComponent {
-  firstName: string;
-  lastName: string;
-  age: number;
-  address;
+  user: User;
 
-  foo: any;
-  hasKids: boolean;
-  // this makes the numberArray variable expect an array of numbers nothing else
-  numberArray: number[];
-  // same concept but with strings
-  stringArray: string[];
-  // can hold any values
-  mixedArray: any[];
-  // defining each index in the array, after the defined amounts you can have whatever data you want without throwing errors
-  myTuple: [string, number, boolean];
-
-  u: undefined;
-  n: null;
-
-  // runs automatically when object is instanciated
   constructor() {
-    this.firstName = 'John';
-    this.lastName = 'Doe';
-    this.age =  30;
-    this.address = {
-      street: '123 Main St',
-      city: 'Mayer',
-      state: 'Minnesota'
+    this.user = {
+      firstName: 'John',
+      lastName: 'Doe',
+      age: 32,
+      address: {
+        street: '123 Main Street',
+        city: 'Mayer',
+        state: 'MN'
+      }
     };
-    this.numberArray = [1, 2, 3];
-    this.myTuple = ['hello', 4, true];
-    this.u = undefined;
-    this.n = null;
-
-    console.log(this.addNumbers(2, 3));
-  }
-
-  // sayHello() {
-  //   console.log(`Hello ${this.firstName}`);
-  // }
-
-  // hasBirthday() {
-  //   this.age += 1;
-  // }
-
-  addNumbers(num1: number, num2: number): number{
-    return num1 + num2;
   }
 }
+
