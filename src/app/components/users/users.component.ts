f4c4f9287bce165e86e7f328c504fb47b9ad266e
Matt <cook6348@gmail.com>
2019-03-08 07:58:52 -0600

    Adding ngFor to project

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +8,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
+  users: User[];
+
   constructor() { }
 
   ngOnInit() {
+    this.users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 32,
+        address: {
+          street: '123 Main Street',
+          city: 'Mayer',
+          state: 'MN'
+        }
+      },
+      {
+        firstName: 'Matt',
+        lastName: 'Cook',
+        age: 32,
+        address: {
+          street: '123 Pinecone Street',
+          city: 'Mayer',
+          state: 'MN'
+        }
+      },
+      {
+        firstName: 'Jim',
+        lastName: 'Bob',
+        age: 32,
+        address: {
+          street: '123 Hillbilly Street',
+          city: 'Mayer',
+          state: 'MN'
+        }
+      }
+    ];
+
+    this.addUser({
+      firstName: 'David',
+      lastName: 'Bo',
+      age: 31,
+      address: {
+        street: '123 Hillbilly Street',
+        city: 'Mayer',
+        state: 'MN'
+      }
+    })
   }
 
+    addUser(user: User) {
+      this.users.push(user);
+    }
 }
