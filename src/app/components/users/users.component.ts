a8765b225e880a7b1e1da465d4ea9dd05d3d8f20
Matt <cook6348@gmail.com>
2019-03-10 10:26:04 -0500

    cleaned up files to start working with events

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { User } from '../../models/User'; 
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-users',
@@ -12,88 +12,59 @@ export class UsersComponent implements OnInit {
   showExtended: boolean = true;
   loaded: boolean = false;
   enableAdd: boolean = true;
-  currentClasses ={
-
-  }
-  currentStyles = {
-
-  }
 
   constructor() { }
 
   ngOnInit() {
-   
-      this.users = [
-        {
-          firstName: 'Matt',
-          lastName: 'Cook',
-          age: 32,
-          address: {
-            street: '50 Main st',
-            city: 'Mayer',
-            state: 'MN'
-          },
-          image: 'http://lorempixel.com/600/600/people/6',
-          isActive: true,
-          balance: 100,
-          registered: new Date('01/02/2018 08:30:00')
-        },
-        {
-          firstName: 'Danelle',
-          lastName: 'Cook',
-          age: 34,
-          address: {
-            street: '20 School st',
-            city: 'Mayer',
-            state: 'MN'
-          },
-          image: 'http://lorempixel.com/600/600/people/8',
-          isActive: false,
-          balance: 200,
-          registered: new Date('05/02/2018 02:30:00')
-        },
-        {
-          firstName: 'Mike',
-          lastName: 'Williams',
-          age: 26,
-          address: {
-            street: '55 Mill st',
-            city: 'Montrose',
-            state: 'MN'
-          },
-          image: 'http://lorempixel.com/600/600/people/9',
-          isActive: true,
-          balance: 600,
-          registered: new Date('08/02/2018 11:10:00')
-        }
-      ];
 
-      this.loaded = true;
+    this.users = [
+      {
+        firstName: 'Matt',
+        lastName: 'Cook',
+        age: 32,
+        address: {
+          street: '50 Main st',
+          city: 'Mayer',
+          state: 'MN'
+        },
+        isActive: true,
+        registered: new Date('01/02/2018 08:30:00')
+      },
+      {
+        firstName: 'Danelle',
+        lastName: 'Cook',
+        age: 34,
+        address: {
+          street: '20 School st',
+          city: 'Mayer',
+          state: 'MN'
+        },
 
-      // this.addUser({
-      //   firstName: 'David',
-      //   lastName: 'Jackson'
-      // });
-      this.setCurrentClasses();
-      this.setCurrentStyles();
+        isActive: false,
+
+        registered: new Date('05/02/2018 02:30:00')
+      },
+      {
+        firstName: 'Mike',
+        lastName: 'Williams',
+        age: 26,
+        address: {
+          street: '55 Mill st',
+          city: 'Montrose',
+          state: 'MN'
+        },
+
+        isActive: true,
+
+        registered: new Date('08/02/2018 11:10:00')
+      }
+    ];
+
+    this.loaded = true;
   }
 
   addUser(user: User) {
     this.users.push(user);
   }
 
-  setCurrentClasses() {
-    this.currentClasses = {
-      'btn-success': this.enableAdd,
-      'big-text': this.showExtended
-    }
-  }
-
-  setCurrentStyles() {
-    this.currentStyles = {
-      'padding-top': this.showExtended ? '0' : '140px',
-      'font-size': this.showExtended ? '' : '30px'
-    }
-  }
-  
 }
